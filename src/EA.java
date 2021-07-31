cd868adc52fd61c8b0cad4ec1e54c8d47681e440
Michael Ding <michaelding2233@gmail.com>
2021-07-31 12:10:30 -0700

    formatting

--- src/EA.java
+++ src/EA.java
@@ -18,68 +18,79 @@ import org.apache.pdfbox.text.PDFTextStripper;
  * Michael Ding, 2021
  */
 
-public class EA{	
+public class EA {
 	public static void main(String args[]) throws IOException {
-		//location of folder with assessments
+		// location of folder with assessments
 		String dir = "C:/Users/micha/OneDrive/Desktop/New York Invasive Species Research Institute/data/Assessments_forMD/EA/";
-		
-		//get array of files
+
+		// get array of files
 		File file = new File(dir);
-        File[] files = file.listFiles();    
-        
-        //headers for csv file
-    	String[] HEADERS = {"scientificName", "Other scientific names", "stateCommonName", "Form type", "1.1 score", "1.2 score", "1.3 score", "1.4 score"};
-    	
-    	//output file
-    	FileWriter out = new FileWriter("outputEA.csv");
-        CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS));
-        
-        //for each file in the dir
-    	for (int i = 0; i < files.length; i++) {
-        	System.out.println(files[i].getName());
-        	if(!files[i].getName().equals("desktop.ini"))
-	        	extract(dir + files[i].getName(), printer);		
-        }
+		File[] files = file.listFiles();
+
+		// headers for csv file
+		String[] HEADERS = { "scientificName", "Other scientific names", "stateCommonName", "Form type", "1.1 score",
+				"1.2 score", "1.3 score", "1.4 score" };
+
+		// output file
+		FileWriter out = new FileWriter("outputEA.csv");
+		CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS));
+
+		// for each file in the dir
+		for (int i = 0; i < files.length; i++) {
+			System.out.println(files[i].getName());
+			if (!files[i].getName().equals("desktop.ini"))
+				extract(dir + files[i].getName(), printer);
+		}
 	}
-	
+
 	public static void extract(String filePath, CSVPrinter printer) throws IOException {
-		//read pdf
+		// read pdf
 		PDDocument document = PDDocument.load(new File(filePath));
 		PDFTextStripper pdfStripper = new PDFTextStripper();
-  		String text = pdfStripper.getText(document);
-  		
-        //getting names with indexof() and substring()
-        String scientificName = text.substring(text.indexOf("Scientific name:") + 16, text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific name:") + 18) + 2)).trim();
-        scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
-        String otherScientificName = text.substring(text.indexOf("Scientific name:") + 16, text.indexOf("Common names:")).trim();
-        String commonNames = text.substring(text.indexOf("Common names:") + 13, text.indexOf("Native distribution:")).trim();
-        
-        //get form type
-        String formType = text.substring(text.indexOf("NEW YORK") + 8, text.indexOf("Scientific name:")).trim();
-        formType = formType.substring(0, formType.length() - 1).trim();
-        
-        //double array for our scores which will be scaled from 0-1
-        double[] scores = new double[5];
-        
-        //whether or not the assessment has a 1.4
-        int n = 3; if(text.indexOf("1.4. Impact") != -1) n++;
-        
-        /*we get the scores for 1.1, 1.2, 1.3 and 1.4 if that exists
-        	blanks and "U"'s are represented as a 0*/
-        for(int i = 1; i <= n; i++) {
-        	int loc = text.indexOf("Score", text.indexOf("1." + String.valueOf(i) + ". Impact")) + 5;
-	        String temp = text.substring(loc, loc + 3).trim().replaceAll("[^0-9]","");
-	        if(temp.equals("U")) scores[i] = 0;
-	        else if(temp.equals("")) scores[i] = 0;
-	        else scores[i] = Integer.parseInt(temp)/10.0;
-        }
-                	        
-        //print data to csv
-  	    printer.printRecord(scientificName, otherScientificName, commonNames, formType, scores[1], scores[2], scores[3], scores[4]);
-  	    
-  	    //flush and close
-  	    printer.flush();
-  	    document.close();
+		String text = pdfStripper.getText(document);
+
+		// getting names with indexof() and substring()
+		String scientificName = text.substring(text.indexOf("Scientific name:") + 16,
+				text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific name:") + 18) + 2)).trim();
+		scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
+		String otherScientificName = text
+				.substring(text.indexOf("Scientific name:") + 16, text.indexOf("Common names:")).trim();
+		String commonNames = text.substring(text.indexOf("Common names:") + 13, text.indexOf("Native distribution:"))
+				.trim();
+
+		// get form type
+		String formType = text.substring(text.indexOf("NEW YORK") + 8, text.indexOf("Scientific name:")).trim();
+		formType = formType.substring(0, formType.length() - 1).trim();
+
+		// double array for our scores which will be scaled from 0-1
+		double[] scores = new double[5];
+
+		// whether or not the assessment has a 1.4
+		int n = 3;
+		if (text.indexOf("1.4. Impact") != -1)
+			n++;
+
+		/*
+		 * we get the scores for 1.1, 1.2, 1.3 and 1.4 if that exists blanks and "U"'s
+		 * are represented as a 0
+		 */
+		for (int i = 1; i <= n; i++) {
+			int loc = text.indexOf("Score", text.indexOf("1." + String.valueOf(i) + ". Impact")) + 5;
+			String temp = text.substring(loc, loc + 3).trim().replaceAll("[^0-9]", "");
+			if (temp.equals("U"))
+				scores[i] = 0;
+			else if (temp.equals(""))
+				scores[i] = 0;
+			else
+				scores[i] = Integer.parseInt(temp) / 10.0;
+		}
+
+		// print data to csv
+		printer.printRecord(scientificName, otherScientificName, commonNames, formType, scores[1], scores[2], scores[3],
+				scores[4]);
+
+		// flush and close
+		printer.flush();
+		document.close();
 	}
 }
-
--- src/SEABlue.java
+++ src/SEABlue.java
@@ -20,71 +20,83 @@ import org.apache.pdfbox.text.PDFTextStripper;
  * Michael Ding, 2021
  */
 
-public class SEABlue{	
-	
-	//lists for forest impact, agriculture and both
-    public static List<String> forestList = new ArrayList<String>();  
-    public static List<String> agricultureList = new ArrayList<String>();  
-    public static List<String> bothList = new ArrayList<String>();  
-	
+public class SEABlue {
+
+	// lists for forest impact, agriculture and both
+	public static List<String> forestList = new ArrayList<String>();
+	public static List<String> agricultureList = new ArrayList<String>();
+	public static List<String> bothList = new ArrayList<String>();
+
 	public static void main(String args[]) throws IOException {
-		//location of folder with assessments
+		// location of folder with assessments
 		String dir = "C:/Users/micha/OneDrive/Desktop/New York Invasive Species Research Institute/data/Assessments_forMD/SEA-blue/";
-		
-		//get array of files
+
+		// get array of files
 		File file = new File(dir);
-        File[] files = file.listFiles();    
-                
-        //headers for extract()
-    	//String[] HEADERS = {"scientificName", "Other scientific names", "stateCommonName", "Hum_health_pos", 
-		//"Hum_health_neg", "Ind1_ag_pos", "Ind2_for_pos", "Ind1_ag_neg", "Ind2_for_neg", "Soc_rec_pos", "Soc_rec_neg"}; 
-    	
-        //headers for check()
-    	String[] HEADERS = {"scientificName", "Other scientific names", "stateCommonName", "type", "sign", "word", "line"};
-    	
-    	//output file
-    	FileWriter out = new FileWriter("outputSEABlue.csv");
-        CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS));        
-                
-        //for each file in the dir
-    	for (int i = 0; i < files.length; i++) {
-        	System.out.println(files[i].getName());
-        	if(!files[i].getName().equals("desktop.ini")) {
-	        	//extract(dir + files[i].getName(), printer);
-        		check(dir + files[i].getName(), printer);
-        	}
-        }
-    	
-    	/*print out the number of each category (forest, agriculture, or both), and list out the names. 
-			the prints will not work for check() because that method does not add species to these lists*/
+		File[] files = file.listFiles();
+
+		// headers for extract()
+		// String[] HEADERS = {"scientificName", "Other scientific names",
+		// "stateCommonName", "Hum_health_pos",
+		// "Hum_health_neg", "Ind1_ag_pos", "Ind2_for_pos", "Ind1_ag_neg",
+		// "Ind2_for_neg", "Soc_rec_pos", "Soc_rec_neg"};
+
+		// headers for check()
+		String[] HEADERS = { "scientificName", "Other scientific names", "stateCommonName", "type", "sign", "word",
+				"line" };
+
+		// output file
+		FileWriter out = new FileWriter("outputSEABlue.csv");
+		CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS));
+
+		// for each file in the dir
+		for (int i = 0; i < files.length; i++) {
+			System.out.println(files[i].getName());
+			if (!files[i].getName().equals("desktop.ini")) {
+				// extract(dir + files[i].getName(), printer);
+				check(dir + files[i].getName(), printer);
+			}
+		}
+
+		/*
+		 * print out the number of each category (forest, agriculture, or both), and
+		 * list out the names. the prints will not work for check() because that method
+		 * does not add species to these lists
+		 */
 		System.out.println("\n\nSEA Blue");
 		System.out.println("forest: " + forestList.size());
-		for(int i = 0; i < forestList.size(); i++) System.out.println(forestList.get(i));
-		
+		for (int i = 0; i < forestList.size(); i++)
+			System.out.println(forestList.get(i));
+
 		System.out.println("\n\n\nagri: " + agricultureList.size());
-		for(int i = 0; i < agricultureList.size(); i++) System.out.println(agricultureList.get(i));
-		
+		for (int i = 0; i < agricultureList.size(); i++)
+			System.out.println(agricultureList.get(i));
+
 		System.out.println("\n\n\nboth: " + bothList.size());
-		for(int i = 0; i < bothList.size(); i++) System.out.println(bothList.get(i));
+		for (int i = 0; i < bothList.size(); i++)
+			System.out.println(bothList.get(i));
 	}
-	
+
 	public static void check(String filePath, CSVPrinter printer) throws IOException {
-		//read pdf
+		// read pdf
 		PDDocument document = PDDocument.load(new File(filePath));
 		PDFTextStripper pdfStripper = new PDFTextStripper();
-  		String text = pdfStripper.getText(document);
-  		
-        //getting names with indexof() and substring()
-        String scientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific Name:") + 18) + 2)).trim();
-        scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
-        String otherScientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf("Common Name:")).trim();
-        String commonNames = text.substring(text.indexOf("Common Name:") + 13, text.indexOf("Native Distribution:")).trim();
-                
-        //list of key words we search with
-        String[] agList = {"agricult", "crop", "farm", "horticult", "honey", "cattle", "livestock"};
-        String[] forList = {"forest", "timber", "lumber", "silvicult", "tree"};
+		String text = pdfStripper.getText(document);
 
-        //making substrings of the discussion section 
+		// getting names with indexof() and substring()
+		String scientificName = text.substring(text.indexOf("Scientific Name:") + 16,
+				text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific Name:") + 18) + 2)).trim();
+		scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
+		String otherScientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf("Common Name:"))
+				.trim();
+		String commonNames = text.substring(text.indexOf("Common Name:") + 13, text.indexOf("Native Distribution:"))
+				.trim();
+
+		// list of key words we search with
+		String[] agList = { "agricult", "crop", "farm", "horticult", "honey", "cattle", "livestock" };
+		String[] forList = { "forest", "timber", "lumber", "silvicult", "tree" };
+
+		// making substrings of the discussion section
 		String posDiscussion = text.substring(
 				text.indexOf("Discussion:", text.indexOf("ECONOMIC VALUE OF THE SPECIES:")),
 				text.indexOf("Sources of Information:", text.indexOf("ECONOMIC VALUE OF THE SPECIES:")));
@@ -92,10 +104,11 @@ public class SEABlue{
 				text.indexOf("Discussion:", text.indexOf("Moderate Detriment (impacts minor and long lasting")),
 				text.indexOf("Sources of Information:",
 						text.indexOf("Moderate Detriment (impacts minor and long lasting")));
-             
-        //for each key word in our agList array, we use .contains() to find it and pint out the sentence that the word is found in
+
+		// for each key word in our agList array, we use .contains() to find it and pint
+		// out the sentence that the word is found in
 		for (int i = 0; i < agList.length; i++)
-			if (posDiscussion.contains(agList[i])) 
+			if (posDiscussion.contains(agList[i]))
 				if (posDiscussion.indexOf(".", posDiscussion.indexOf(agList[i])) == -1)
 					printer.printRecord(scientificName, otherScientificName, commonNames, "agr", "pos", agList[i],
 							posDiscussion
@@ -107,7 +120,7 @@ public class SEABlue{
 									posDiscussion.indexOf(".", posDiscussion.indexOf(agList[i]))));
 
 		for (int i = 0; i < agList.length; i++)
-			if (negDiscussion.contains(agList[i])) 
+			if (negDiscussion.contains(agList[i]))
 				if (negDiscussion.indexOf(".", negDiscussion.indexOf(agList[i])) == -1)
 					printer.printRecord(scientificName, otherScientificName, commonNames, "agr", "neg", agList[i],
 							negDiscussion
@@ -119,7 +132,7 @@ public class SEABlue{
 									negDiscussion.indexOf(".", negDiscussion.indexOf(agList[i]))));
 
 		for (int i = 0; i < forList.length; i++)
-			if (posDiscussion.contains(forList[i])) 
+			if (posDiscussion.contains(forList[i]))
 				if (posDiscussion.indexOf(".", posDiscussion.indexOf(forList[i])) == -1)
 					printer.printRecord(scientificName, otherScientificName, commonNames, "forest", "pos", forList[i],
 							posDiscussion
@@ -131,7 +144,7 @@ public class SEABlue{
 									posDiscussion.indexOf(".", posDiscussion.indexOf(forList[i]))));
 
 		for (int i = 0; i < forList.length; i++)
-			if (negDiscussion.contains(forList[i])) 
+			if (negDiscussion.contains(forList[i]))
 				if (negDiscussion.indexOf(".", negDiscussion.indexOf(forList[i])) == -1)
 					printer.printRecord(scientificName, otherScientificName, commonNames, "forest", "neg", forList[i],
 							negDiscussion
@@ -141,48 +154,58 @@ public class SEABlue{
 							negDiscussion.substring(
 									negDiscussion.lastIndexOf(".", negDiscussion.indexOf(forList[i])) + 1,
 									negDiscussion.indexOf(".", negDiscussion.indexOf(forList[i]))));
-                	    		               
-  	    //flush and close
-  	    printer.flush();
-  	    document.close();
+
+		// flush and close
+		printer.flush();
+		document.close();
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public static void extract(String filePath, CSVPrinter printer) throws IOException {
-		//read pdf
+		// read pdf
 		PDDocument document = PDDocument.load(new File(filePath));
 		PDFTextStripper pdfStripper = new PDFTextStripper();
-  		String text = pdfStripper.getText(document);
-  		
-        //getting names with indexof() and substring()
-        String scientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific Name:") + 18) + 2)).trim();
-        scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
-        String otherScientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf("Common Name:")).trim();
-        String commonNames = text.substring(text.indexOf("Common Name:") + 13, text.indexOf("Native Distribution:")).trim();
-  		
-        String[] arr = text.split("\\s");   //splits our text String with spaces
-        double[] scores = new double[10];  //double array for our scores which will be scaled
-        
-        //get scores by finding "pts"
-        int q = 0;
-        for(int i = 0; i < arr.length; i++) 
-        	if(arr[i].contains("pts")) {
-                scores[q] = Integer.parseInt(arr[i].trim().substring(0, arr[i].trim().length()-3).trim());
-        		q++;
-        	}
-              
-        /*we used the scores array to get the scores but the indexes do not align right now with the order we want to print in
-    		so we rearange the scores and scale them to be from 0 - 1 */
-        scores[0] = scores[0]/15.0; scores[1] = scores[1]/15.0;
-        scores[6] = scores[6]/15.0; scores[7] = scores[7]/15.0;
-        double pos = scores[3]/70.0; double neg = scores[4]/70.0;
-        for(int i = 2; i <= 5; i++) scores[i] = 0;
-        
-        //list of key words we search with
-        String[] agList = {"agricult", "crop", "farm", "horticult", "honey", "cattle", "livestock"};
-        String[] forList = {"forest", "timber", "lumber", "silvicult", "tree"};
+		String text = pdfStripper.getText(document);
 
-        //making substrings of the discussion section 
+		// getting names with indexof() and substring()
+		String scientificName = text.substring(text.indexOf("Scientific Name:") + 16,
+				text.indexOf(" ", text.indexOf(" ", text.indexOf("Scientific Name:") + 18) + 2)).trim();
+		scientificName = scientificName.replaceAll("[^a-zA-Z ]", "");
+		String otherScientificName = text.substring(text.indexOf("Scientific Name:") + 16, text.indexOf("Common Name:"))
+				.trim();
+		String commonNames = text.substring(text.indexOf("Common Name:") + 13, text.indexOf("Native Distribution:"))
+				.trim();
+
+		String[] arr = text.split("\\s"); // splits our text String with spaces
+		double[] scores = new double[10]; // double array for our scores which will be scaled
+
+		// get scores by finding "pts"
+		int q = 0;
+		for (int i = 0; i < arr.length; i++)
+			if (arr[i].contains("pts")) {
+				scores[q] = Integer.parseInt(arr[i].trim().substring(0, arr[i].trim().length() - 3).trim());
+				q++;
+			}
+
+		/*
+		 * we used the scores array to get the scores but the indexes do not align right
+		 * now with the order we want to print in so we rearange the scores and scale
+		 * them to be from 0 - 1
+		 */
+		scores[0] = scores[0] / 15.0;
+		scores[1] = scores[1] / 15.0;
+		scores[6] = scores[6] / 15.0;
+		scores[7] = scores[7] / 15.0;
+		double pos = scores[3] / 70.0;
+		double neg = scores[4] / 70.0;
+		for (int i = 2; i <= 5; i++)
+			scores[i] = 0;
+
+		// list of key words we search with
+		String[] agList = { "agricult", "crop", "farm", "horticult", "honey", "cattle", "livestock" };
+		String[] forList = { "forest", "timber", "lumber", "silvicult", "tree" };
+
+		// making substrings of the discussion section
 		String posDiscussion = text.substring(
 				text.indexOf("Discussion:", text.indexOf("ECONOMIC VALUE OF THE SPECIES:")),
 				text.indexOf("Sources of Information:", text.indexOf("ECONOMIC VALUE OF THE SPECIES:")));
@@ -190,37 +213,49 @@ public class SEABlue{
 				text.indexOf("Discussion:", text.indexOf("Moderate Detriment (impacts minor and long lasting")),
 				text.indexOf("Sources of Information:",
 						text.indexOf("Moderate Detriment (impacts minor and long lasting")));
-              
-        boolean ag = false; boolean forest = false;
 
-        //for each key word in our agList array, we use .contains() to find it
-        for(int i = 0; i < agList.length; i++) if(posDiscussion.contains(agList[i])) {
-        	scores[2] = pos; ag = true;
-        }
-   
-        for(int i = 0; i < agList.length; i++) if(negDiscussion.contains(agList[i])) {
-        	scores[4] = neg; ag = true;
-        }
-                
-        for(int i = 0; i < forList.length; i++) if(posDiscussion.contains(forList[i])) {
-        	scores[3] = pos; forest = true;     
-    	}
-    
-        for(int i = 0; i < forList.length; i++) if(negDiscussion.contains(forList[i])) {
-        	scores[5] = neg; forest = true;
-        }
-                	    		               
-        //adding scientific names to corresponding list
-        if(forest && !ag) forestList.add(scientificName);
-        if(!forest && ag) agricultureList.add(scientificName);
-        if(forest && ag) bothList.add(scientificName);
-        
-        //print data to csv
-  	    printer.printRecord(scientificName, otherScientificName, commonNames, scores[0], scores[1], scores[2], scores[3], scores[4], scores[5], scores[6], scores[7]);
-  	    
-  	    //flush and close
-  	    printer.flush();
-  	    document.close();
+		boolean ag = false;
+		boolean forest = false;
+
+		// for each key word in our agList array, we use .contains() to find it
+		for (int i = 0; i < agList.length; i++)
+			if (posDiscussion.contains(agList[i])) {
+				scores[2] = pos;
+				ag = true;
+			}
+
+		for (int i = 0; i < agList.length; i++)
+			if (negDiscussion.contains(agList[i])) {
+				scores[4] = neg;
+				ag = true;
+			}
+
+		for (int i = 0; i < forList.length; i++)
+			if (posDiscussion.contains(forList[i])) {
+				scores[3] = pos;
+				forest = true;
+			}
+
+		for (int i = 0; i < forList.length; i++)
+			if (negDiscussion.contains(forList[i])) {
+				scores[5] = neg;
+				forest = true;
+			}
+
+		// adding scientific names to corresponding list
+		if (forest && !ag)
+			forestList.add(scientificName);
+		if (!forest && ag)
+			agricultureList.add(scientificName);
+		if (forest && ag)
+			bothList.add(scientificName);
+
+		// print data to csv
+		printer.printRecord(scientificName, otherScientificName, commonNames, scores[0], scores[1], scores[2],
+				scores[3], scores[4], scores[5], scores[6], scores[7]);
+
+		// flush and close
+		printer.flush();
+		document.close();
 	}
 }
-
